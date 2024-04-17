4bff42d4096736a22b469a97b6c2c1957df20f07
lev <levbayanov@gmai.com>
2024-04-17 15:03:09 +0500

    Добавил класс Snake, нужно всю логику работы со змею перенести туда

---
+++ src/Snake.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Snake {
+    private Image body;
+    private Image Head;
+
+    public Snake(Image body, Image Head) {
+        this.body = body;
+        this.Head = Head;
+    }
+
+}
